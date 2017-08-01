d5124fbab7816b707c2f6a204af75fc3a2da7a79
Michael Felipe Rondón Acosta <mfrondona@unal.edu.co>
2017-07-31 21:33:12 -0500

    Se agrega docker compose para intentar usar swarm
    Se agrega docker compose para intentar usar swarm

--- servicios/start.sh
+++ servicios/start.sh
@@ -1,6 +1,6 @@
 cd ProductService
 mvn clean package
 cd ..
-# docker-compose up -d --build
-docker swarm init
-docker stack deploy -c docker-compose.yml kallsonys
+docker-compose up -d --build
+#docker swarm init
+#docker stack deploy -c docker-compose.yml kallsonys
