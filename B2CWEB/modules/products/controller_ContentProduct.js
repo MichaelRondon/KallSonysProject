a61353badd97cb555ae247fe2a6ac684a30748fb
Michael Felipe Rondón Acosta <mfrondona@unal.edu.co>
2017-10-31 22:06:47 -0500

    Busqueda productos scroll linea base
    Búsqueda productos scroll línea base

---
+++ B2CWEB/modules/products/controller_ContentProduct.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+angular.module('Products')
+.controller("controller_ContentProduct", 
+	['$scope', '$location', '$http',
+    function ($scope, $location, $http) {
+			var objsURL = $location.search();
+			var objsURL = objsURL.Id;
+			
+			
+}]);
+	
+
+
